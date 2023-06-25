8f4dffd614b0f1951a07b8386fd5b663068bb6d0
Pedro dos Anjos <pedrodeanjos@gmail.com>
2023-06-24 23:32:00 -0300

    Props

--- Introducao React/introducao/src/App.js
+++ Introducao React/introducao/src/App.js
@@ -1,13 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
 import HelloWorld from './components/HelloWorld';
+import SayMyName from './components/SayMyName';
+import Pessoa from './components/Pessoa';
 
 function App() {
+  const nome = 'Joao' 
+
   return (
     <div className="App">
-     <h2>Alterando o JSX</h2>
-     <p>Olá Mundo</p>
      <HelloWorld />
+     <SayMyName nome='Pedro'/>
+     <SayMyName nome='Henrique'/>
+     <SayMyName nome={nome}/>
+     <Pessoa nome='Rodrigo' idade='18' profissao='Programador' foto='https://via.placeholder.com/150'/>
+
+
     </div>
     
   );

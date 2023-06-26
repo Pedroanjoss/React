65b0452b6044ca01ea601d9f266c7223ac21b8b9
Pedro dos Anjos <pedrodeanjos@gmail.com>
2023-06-25 23:35:59 -0300

    Adicionando CSS

--- Introducao React/introducao/src/App.js
+++ Introducao React/introducao/src/App.js
@@ -9,6 +9,7 @@ function App() {
 
   return (
     <div className="App">
+      <h1>Teste css </h1>
      <HelloWorld />
      <SayMyName nome='Pedro'/>
      <SayMyName nome='Henrique'/>
--- Introducao React/introducao/src/components/Pessoa.js
+++ Introducao React/introducao/src/components/Pessoa.js
@@ -1,4 +1,4 @@
-function Pessoa(nome, idade, profissao, foto) {
+function Pessoa({nome, idade, profissao, foto}) {
     return( 
     <div>
         <img src={foto} alt={nome} />

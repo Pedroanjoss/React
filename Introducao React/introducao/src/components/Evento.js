4af0a27277148de6e2acfde45c78cf56c885c8c7
Pedro dos Anjos <pedrodeanjos@gmail.com>
2023-06-29 16:33:55 -0300

    Passando eventos por props

--- Introducao React/introducao/src/components/Evento.js
+++ Introducao React/introducao/src/components/Evento.js
@@ -1,13 +1,21 @@
+import Button from "./Evento/Button"
+
 function Evento(){
 
    function meuEvento (){
-    console.log('Opa fui ativado')
+    console.log('Ativando primeiro evento')
+   }
+
+   function segundoEvento() {
+    console.log('Ativando segundo evento')
    }
 
     return(
         <div>
             <p>Clique aqui para ativar um evento:</p>
-            <button onClick={meuEvento} >Ativar!</button>
+            <Button event={meuEvento} text="Primeiro Evento" />
+            <Button event={segundoEvento} text="Segundo Evento" />
+           
         </div>
     )
 }
